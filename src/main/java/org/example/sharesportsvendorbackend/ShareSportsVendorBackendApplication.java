d73abc8d6a8511ef4f7185b6b02f3340c3ebea9d
lazarus0320 <astar5327z@gmail.com>
2024-11-06 12:12:28 +0900

    Feat: first commit

---
+++ src/main/java/org/example/sharesportsvendorbackend/ShareSportsVendorBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.sharesportsvendorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareSportsVendorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareSportsVendorBackendApplication.class, args);
+	}
+
+}
